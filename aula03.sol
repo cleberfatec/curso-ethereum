a74c8d37bb33678d76b636eaacb35b28237d83e8
Cleber <cleberfatec@yahoo.com.br>
2023-06-12 15:29:35 -0300

    refatoração

--- aula03.sol
+++ aula03.sol
@@ -4,39 +4,55 @@ pragma solidity 0.8.18;
 //0xE86eAEd3dAa1A831102445d187c3409de92da1Bb
 
 contract Aluguel {
-    string private locador;
-    string private locatario;
-    uint256[36] private valorAluguel;
+    
+    struct AluguelEstrutura {
+        string locador;
+        string locatario;
+        uint256[36] valorAluguel;
+    }
+
+    AluguelEstrutura private aluguel;
 
     constructor(string memory _locador, string memory _locatario, uint256 _valorAluguel) {
-        locador = _locador;
-        locatario = _locatario;
+        aluguel.locador = _locador;
+        aluguel.locatario = _locatario;        
         for (uint256 i = 0; i < 36; i++) {
-            valorAluguel[i] = _valorAluguel;
+            aluguel.valorAluguel[i] = _valorAluguel;
         }    
     }
 
-    function getValorAluguelPorMes(uint256 _mes) public view returns (uint256) {
-        require(_mes > 0 && _mes <= 36, "Mes invalido");
-        return valorAluguel[_mes - 1];
+    function getValorAluguelPorMes(uint256 _mes) 
+    public 
+    view 
+    mesValido(_mes) 
+    returns (uint256) {        
+        
+        return aluguel.valorAluguel[_mes - 1];
     }
 
     function getLocadorLocatario() public view returns (string memory, string memory) {
-        return (locador, locatario);
+        return (aluguel.locador, aluguel.locatario);
     }
 
     function setNomes(uint256 _tipo, string memory nome) external {
         if (_tipo == 1) {
-            locador = nome;
+            aluguel.locador = nome;
         } else if (_tipo == 2) {
-            locatario = nome;
+            aluguel.locatario = nome;
         }
     }
 
-    function ajustarAluguel(uint256 valor, uint256 mesInicial) public {
-        require(mesInicial > 0 && mesInicial < 36, "Mes invalido");
+    function ajustarAluguel(uint256 valor, uint256 mesInicial) 
+    public 
+    mesValido(mesInicial)
+    {
         for (uint256 i = mesInicial; i < 36; i++) {
-            valorAluguel[i] += valor;
+            aluguel.valorAluguel[i] += valor;
         }
+    }    
+
+    modifier mesValido(uint256 _mes) {
+        require(_mes > 0 && _mes <= 36, "Mes invalido");
+        _;
     }
 }
